210df4a5d5d2c58ad2f74b93bbe3a4439eaa445c
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-05-23 17:09:41 +0200

    updated global types for analytics and promptevent

--- svelte-app/src/global.d.ts
+++ svelte-app/src/global.d.ts
@@ -1,3 +1,35 @@
 /// <reference types="@sveltejs/kit" />
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+/// <reference types="google.analytics" />
+// /// <reference types="@stripe/stripe-js" />
+
+/**
+ * The BeforeInstallPromptEvent is fired at the Window.onbeforeinstallprompt handler
+ * before a user is prompted to "install" a web site to a home screen on mobile.
+ *
+ * Only supported on Chrome and Android Webview.
+ */
+interface BeforeInstallPromptEvent extends Event {
+	/**
+	 * Returns an array of DOMString items containing the platforms on which the event was dispatched.
+	 * This is provided for user agents that want to present a choice of versions to the user such as,
+	 * for example, "web" or "play" which would allow the user to chose between a web version or
+	 * an Android version.
+	 */
+	readonly platforms: Array<string>;
+
+	/**
+	 * Returns a Promise that resolves to a DOMString containing either "accepted" or "dismissed".
+	 */
+	readonly userChoice: Promise<{
+		outcome: 'accepted' | 'dismissed';
+		platform: string;
+	}>;
+
+	/**
+	 * Allows a developer to show the install prompt at a time of their own choosing.
+	 * This method returns a Promise.
+	 */
+	prompt(): Promise<void>;
+}

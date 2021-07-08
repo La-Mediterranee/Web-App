29bde700ee685ef561f2bc178d129b7d94fea74f
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-08 11:10:42 +0200

    layout update to new api

---
+++ svelte-app/src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import '../app.css';
+	import Navbar from '$lib/components/Navbar/Navbar.svelte';
+	import Footer from '$lib/components/Footer/Footer.svelte';
+	import Installprompt from '$lib/components/Installprompt';
+	import MaterialApp from 'svelte-materialify/src/components/MaterialApp/MaterialAppMin.svelte';
+	// import { FirebaseApp } from '$lib/Firebase';
+	import FirebaseApp from '$lib/Firebase/Firebase.svelte';
+	import Statusbar from '$lib/components/Statusbar';
+
+	let online: boolean;
+</script>
+
+<svelte:window bind:online />
+
+<FirebaseApp>
+	<MaterialApp theme="custom">
+		<Statusbar {online} />
+		<Navbar />
+		<main>
+			<slot />
+		</main>
+		<Installprompt />
+		<Footer />
+	</MaterialApp>
+</FirebaseApp>

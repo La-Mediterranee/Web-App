db36a83fe6f144a4f1302332b591e388cbcdd6cf
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-16 22:44:33 +0200

    constants

---
+++ svelte-app/src/utils/constants.ts
@@ -0,0 +1,12 @@
+export const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
+export const NODE_ENV = import.meta.env.NODE_ENV;
+
+export const firebaseConfig = {
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};

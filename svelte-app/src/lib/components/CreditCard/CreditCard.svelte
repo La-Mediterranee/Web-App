6f59d418f774eb6a0b60711884be56e03dcfa176
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-18 14:42:19 +0200

    creditcard component created

---
+++ svelte-app/src/lib/components/CreditCard/CreditCard.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let card;
+	const { brand, last4, exp_month, exp_year } = card;
+</script>
+
+<option>
+	{brand} **** **** **** {last4} expires {exp_month}/{exp_year}
+</option>
+
+<style>
+	/* your styles go here */
+</style>

ab547561983227233d1b83a411bca95355bf515a
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-18 12:49:11 +0200

    created pages for stripe

---
+++ svelte-app/src/routes/cart/success.svelte
@@ -0,0 +1,10 @@
+<script>
+	const url = window.location.href;
+	const sessionId = new URL(url).searchParams.get('session_id');
+</script>
+
+<h3>Checkout was a Succcess! {sessionId}</h3>
+
+<style>
+	/* your styles go here */
+</style>

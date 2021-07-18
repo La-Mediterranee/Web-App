07da53709fa390d0ebf7d011fcfd47beaeb27084
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-18 14:46:32 +0200

    Profile page

---
+++ svelte-app/src/lib/components/Customer/Profile/Profile.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { signOut } from '@firebase/auth';
+	import type { User, Auth } from 'firebase/auth';
+
+	export let user: User | null = null;
+
+	export let auth: Auth;
+</script>
+
+<div>
+	<img src={user?.photoURL ?? ''} alt={`${user?.displayName ?? 'User'} Profilbild`} />
+
+	<button on:click={() => signOut(auth)} />
+</div>
+
+<style>
+	/* your styles go here */
+</style>

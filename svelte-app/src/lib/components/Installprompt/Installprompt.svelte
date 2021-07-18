2fecf720c9b3dc10f50f611e071d53a5a0bf0537
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-18 14:52:33 +0200

    created installprompt component

--- svelte-app/src/lib/components/Installprompt/Installprompt.svelte
+++ svelte-app/src/lib/components/Installprompt/Installprompt.svelte
@@ -1,55 +1,40 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { browser } from '$app/env';
+	import { onDestroy, onMount } from 'svelte';
 
-	let show = false;
-	let deferedPrompt: BeforeInstallPromptEvent;
-	let showInstallPromo = false;
+	import { createInstallStore } from '$lib/stores/install';
 
-	onMount(() => {
-		window.addEventListener('beforeinstallprompt', (e: BeforeInstallPromptEvent) => {
-			e.preventDefault();
-			deferedPrompt = e;
-			showInstallPromo = true;
-			show = true;
-			// showInstallPromo();
-			// ga('send', 'event', {
-			// 	eventCategory: 'pwa-install',
-			// 	eventAction: 'promo-shown',
-			// 	nonInteraction: true
-			// });
-		});
+	import type { InstallStore } from '$lib/stores/install';
+	import type { Unsubscriber } from 'svelte/store';
 
-		window.addEventListener('appinstalled', () => {
-			// Hide the app-provided install promotion
-			showInstallPromo = false;
-			// Clear the deferredPrompt so it can be garbage collected
-			deferedPrompt = null;
-			// Optionally, send analytics event to indicate successful install
-			console.log('PWA was installed');
-		});
-	});
+	export let installSource: string;
+
+	let store: InstallStore;
+	let unsub: Unsubscriber;
+	let showInstallPromo: boolean;
+
+	if (browser) {
+		console.log(browser);
+		store = createInstallStore(installSource);
+		unsub = store.subscribe((v) => (showInstallPromo = v));
+	}
+
+	if (browser) {
+		onDestroy(() => unsub());
+	}
 
 	async function installPWA(_e: Event) {
-		// Add code show install prompt & hide the install button.
-		const promptEvent = deferedPrompt;
-		if (!promptEvent) {
-			// The deferred prompt isn't available.
-			return;
+		if (await store.install()) {
+			store.set(false);
 		}
-		promptEvent.prompt();
-		// Log user response to prompt.
-		const choice = await promptEvent.userChoice;
-		console.log(`User ${choice.outcome} the A2HS prompt`);
-
-		deferedPrompt = null;
-		// Hide the install button, it can't be called twice.
-		show = true;
 	}
 </script>
 
-<div hidden={showInstallPromo}>
-	<button type="button" on:click={installPWA}> Install </button>
-</div>
+{#if showInstallPromo}
+	<div>
+		<button type="button" on:click={installPWA}> Install </button>
+	</div>
+{/if}
 
 <style>
 	/* your styles go here */

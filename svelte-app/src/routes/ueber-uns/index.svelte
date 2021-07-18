052d45812d7f16871d4303c72bd96cafc66c3b21
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-18 14:38:02 +0200

    about us page created

---
+++ svelte-app/src/routes/ueber-uns/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// export const hydrate = false;
+</script>
+
+<h2>LA-Mediterranee</h2>
+
+<style>
+	/* your styles go here */
+</style>

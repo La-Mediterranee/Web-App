cdae04d48418ff3dc4b4c8e9ff9fffa466ec1a84
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-18 14:44:28 +0200

    removed

--- svelte-app/src/lib/stores/stripe.ts
+++
@@ -1,8 +0,0 @@
-import { readable } from 'svelte/store';
-import { loadStripe } from '@stripe/stripe-js';
-
-export function createStore() {
-	const stripe = await loadStripe(
-		'pk_test_51IsAVoEBqS7ZzpBPUULWoOWoAa3kwI4PBidbVa1qWO9Xk9ELB82exXCnVqsdsBWL2PYhGD2r1LKSdQgXKAKhimdr00x0ZYKGdg'
-	);
-}

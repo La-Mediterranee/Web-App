31b8fe8884f1d437f2c7260f1b8fdcf3792f54f3
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-18 14:58:51 +0200

    signup component

---
+++ svelte-app/src/lib/components/Customer/SignUp/SignUp.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	// [START auth_signup_password_modular]
+	import { getAuth, createUserWithEmailAndPassword } from '@firebase/auth';
+
+	const auth = getAuth();
+
+	async function signUp(email: string, password: string) {
+		try {
+			const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+			const user = userCredential.user;
+		} catch (error) {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+		}
+	}
+	// [END auth_signup_password_modular]
+</script>
+
+<!-- markup (zero or more items) goes here -->
+<style>
+	/* your styles go here */
+</style>

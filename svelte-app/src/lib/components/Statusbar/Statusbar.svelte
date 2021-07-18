3d4479e704218fd966c5c24068612f7ea010820a
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-18 12:53:36 +0200

    online/offline statusbar created

---
+++ svelte-app/src/lib/components/Statusbar/Statusbar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let online = true;
+</script>
+
+<div class:offline={online === false}>
+	<em>Sie sind derzeit offline</em>
+</div>
+
+<style>
+	div {
+		height: 2.5em;
+		width: 100%;
+		z-index: 100;
+		background-color: darkcyan;
+		position: fixed;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		transform: translateY(-100%);
+		transition: transform 0.5s ease-in-out;
+	}
+
+	.offline {
+		transform: translateY(0);
+	}
+</style>

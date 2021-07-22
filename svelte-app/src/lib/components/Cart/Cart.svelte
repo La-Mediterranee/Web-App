8cf0eedc562476554af79a6c1c76dcadf3b971e0
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-22 17:02:02 +0200

    Cart Components created

---
+++ svelte-app/src/lib/components/Cart/Cart.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { cart } from '$lib/stores/cart';
+	import CartItem from './CartItem.svelte';
+</script>
+
+<svelte:head>
+	<meta name="robots" content="noindex, nofollow" />
+</svelte:head>
+
+<h1>Warenkorb</h1>
+
+<div id="cart">
+	{#each $cart as item (item.name)}
+		<CartItem {item} quantity={item.quantity} />
+	{/each}
+</div>
+
+<style>
+	/* your styles go here */
+</style>
---
+++ svelte-app/src/lib/components/Cart/CartItem.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let item: Product;
+	export let quantity: number;
+
+	const { image, name, price, sku } = item;
+
+	$: sum = price * quantity;
+</script>
+
+<div itemscope itemtype="https://schema.org/Product">
+	<img itemprop="image" decoding="async" class="ml-auto" src={image?.src} alt={image?.alt} />
+	<h4 itemprop="name" class="justify-center h4">{name}</h4>
+	<span itemprop="priceCurrency" content="EUR">€</span>
+	<span class="price" itemprop="price" content={`price`}>{sum}</span>
+</div>
+
+<style>
+	/* your styles go here */
+</style>

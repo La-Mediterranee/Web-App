96042661248eac9bfcec9f93c56d361a4036bb67
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-22 17:03:06 +0200

    schema org component

---
+++ svelte-app/src/lib/components/LDTag/LDTag.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { serializeSchema } from '../../utils/json-ld';
+
+	import type { Schema } from 'types/utils/json-ld';
+
+	export let schema: Schema;
+</script>
+
+<svelte:head>
+	{@html serializeSchema(schema)}
+</svelte:head>

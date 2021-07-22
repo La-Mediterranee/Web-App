3bb93b060ccb5e6fa3e78ca06b9ad76b6512f9ed
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-22 17:13:56 +0200

    created first template

--- svelte-app/src/lib/components/Customer/Profile/Profile.svelte
+++ svelte-app/src/lib/components/Customer/Profile/Profile.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+	import { getFirebaseContext } from '$lib/firebase/helpers';
+
 	import { signOut } from '@firebase/auth';
 	import type { User, Auth } from 'firebase/auth';
+	import { getContext } from 'svelte';
 
 	export let user: User | null = null;
 
-	export let auth: Auth;
+	export let auth: Auth = getContext('customer');
+	console.log(auth);
 </script>
 
 <div>

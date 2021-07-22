a30742a012ed79d5d574f10267e85d83061ebaa5
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-22 18:08:34 +0200

    Profile page

---
+++ svelte-app/src/routes/customer/index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { browser } from '$app/env';
+
+	import Profile from '$lib/components/Customer/Profile';
+
+	import type { User } from 'firebase/auth';
+
+	let user: User | null = null;
+
+	// Thinking about making this serverside redirect
+	// check in a load function if there is a firebase token, if not redirect to login page
+	if (browser) {
+		if (!user) {
+			goto('/customer/login');
+		}
+	}
+</script>
+
+<Profile {user} />

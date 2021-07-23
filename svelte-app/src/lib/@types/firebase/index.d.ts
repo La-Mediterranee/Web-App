7c9a2e032f499f0b8092f5934abc2f008fea8662
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-23 10:33:21 +0200

    get auth context added

--- svelte-app/src/lib/@types/firebase/index.d.ts
+++ svelte-app/src/lib/@types/firebase/index.d.ts
@@ -1,11 +1,25 @@
 import type { CollectionReference, DocumentData, Query } from 'firebase/firestore';
 import type { FirebaseApp } from 'firebase/app';
+import type { Auth } from 'firebase/auth';
+import type { Unsubscriber, Subscriber } from 'svelte/store';
+
+declare type Invalidator<T> = (value?: T) => void;
 
 interface Options<T> {
 	startWith?: T;
 	log?: boolean;
 	traceId?: string;
 	once?: boolean;
+}
+
+interface FirebaseUser {
+	subscribe: (
+		this: void,
+		run: Subscriber<any>,
+		invalidate?: Invalidator<any> | undefined
+	) => Unsubscriber;
+	logOut: () => Promise<void>;
+	auth: Auth;
 }
 
 interface StorageOptions extends Options<Object> {
@@ -16,4 +30,8 @@ type QueryFn = (ref: CollectionReference<DocumentData>) => Query;
 
 interface FirebaseContext {
 	getFirebase: () => FirebaseApp;
+}
+
+interface FirebaseAuthContext {
+	getAuth: () => FirebaseUser;
 }

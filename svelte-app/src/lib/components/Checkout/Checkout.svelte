3d392a4141bec4ead9d7b670890c35bb0c005c88
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-24 13:38:16 +0200

    payment chekout component

---
+++ svelte-app/src/lib/components/Checkout/Checkout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import ExpansionPanel from 'svelte-materialify/src/components/ExpansionPanels/ExpansionPanel.svelte';
+	import ExpansionPanels from 'svelte-materialify/src/components/ExpansionPanels/ExpansionPanels.svelte';
+
+	let value = [0];
+	let customerInfo: HTMLFormElement | null = null;
+</script>
+
+<h1>Checkout</h1>
+
+<ExpansionPanels bind:value>
+	<ExpansionPanel>
+		<span slot="header">1.Information</span>
+		<form bind:this={customerInfo} action="/checkout-info">
+			<input type="text" />
+		</form>
+		<button type="button" on:click={() => (value = [1])}>Weiter zur Zahlung</button>
+	</ExpansionPanel>
+	<ExpansionPanel disabled>
+		<span slot="header">2.Zahlung</span>
+		<section>
+			<h3>Trinkgeld hinzufügen</h3>
+			<button type="button" on:click={() => (value = [2])}>Weiter zur Zusammenfassung</button>
+		</section>
+	</ExpansionPanel>
+	<ExpansionPanel disabled>
+		<span slot="header">3.Zusammenfassung</span>
+	</ExpansionPanel>
+</ExpansionPanels>
+
+<style>
+	/* your styles go here */
+</style>

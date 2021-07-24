30f150a99e10e4599082ba717730f56e0f8ed5bf
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-24 13:40:45 +0200

    export added

--- svelte-app/src/lib/firebase/index.ts
+++ svelte-app/src/lib/firebase/index.ts
@@ -1,6 +1,8 @@
 import Auth from './Auth.svelte';
+import AuthSSR from './AuthSSR.svelte';
+
 import FirebaseApp from './Firebase.svelte';
 
 // export const FirebaseApp = Firebase;
-export { FirebaseApp, Auth };
+export { FirebaseApp, Auth, AuthSSR };
 // export default FirebaseApp;

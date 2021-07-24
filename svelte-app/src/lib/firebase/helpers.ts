aadab75cd7595b4e8034d65653b5626b7bb7596e
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-24 13:43:40 +0200

    another helper for context

--- svelte-app/src/lib/firebase/helpers.ts
+++ svelte-app/src/lib/firebase/helpers.ts
@@ -1,7 +1,11 @@
 import { getContext } from 'svelte';
 
-import type { FirebaseContext } from 'types/firebase';
+import type { FirebaseContext, FirebaseAuthContext } from 'types/firebase';
 
 export function getFirebaseContext() {
 	return getContext<FirebaseContext>('firebase').getFirebase();
+}
+
+export function getAuthContext() {
+	return getContext<FirebaseAuthContext>('user')?.getAuth();
 }

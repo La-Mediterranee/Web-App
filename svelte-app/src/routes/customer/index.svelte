82da94296c496c5211e6a97be39399e6272267a8
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-24 13:44:57 +0200

    using auth store

--- svelte-app/src/routes/customer/index.svelte
+++ svelte-app/src/routes/customer/index.svelte
@@ -2,16 +2,15 @@
 	import { goto } from '$app/navigation';
 	import { browser } from '$app/env';
 
+	import { getAuthContext } from '$lib/firebase/helpers';
 	import Profile from '$lib/components/Customer/Profile';
 
-	import type { User } from 'firebase/auth';
-
-	let user: User | null = null;
+	const user = getAuthContext();
 
 	// Thinking about making this serverside redirect
 	// check in a load function if there is a firebase token, if not redirect to login page
 	if (browser) {
-		if (!user) {
+		if (!$user) {
 			goto('/customer/login');
 		}
 	}

f2b707e002be404f02526a7e6cd33ed0b1d654a8
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-24 13:37:27 +0200

    new icons

--- svelte-app/src/lib/Icons/filled/Plus.svelte
+++ svelte-app/src/lib/Icons/filled/Plus.svelte
@@ -7,6 +7,6 @@
 	let _width = `${width}px`;
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" width={_width} height={_height} viewBox="0 0 24 24">
+<svg width={_width} height={_height} viewBox="0 0 24 24">
 	<path fill={color} d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
 </svg>

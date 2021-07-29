0f9d8a76709366ddebd61f801a2735248ae5b7fb
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-29 23:38:13 +0200

    config

--- server/src/config/stripe.ts
+++ server/src/config/stripe.ts
@@ -1,15 +1,10 @@
 import Stripe from 'stripe';
-import { config } from 'dotenv';
-import { NODE_ENV, PORT, STRIPE_SECRET } from '@consts';
+import { STRIPE_SECRET } from '@util/consts';
 
-if (NODE_ENV !== 'production') {
-	config();
-}
+// if (NODE_ENV !== 'production') {
+// 	config();
+// }
 
 export const stripe = new Stripe(STRIPE_SECRET, {
 	apiVersion: '2020-08-27'
 });
-
-import { app } from '../api';
-const port = PORT || 3333;
-app.listen(port, () => console.log(`Listening http://localhost${port}`));

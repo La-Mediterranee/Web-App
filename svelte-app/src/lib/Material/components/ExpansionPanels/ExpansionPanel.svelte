de737ffb28cea7230b7714dafab1c83489cb33ce
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-29 23:41:28 +0200

    added typing

--- svelte-app/src/lib/Material/components/ExpansionPanels/ExpansionPanel.svelte
+++ svelte-app/src/lib/Material/components/ExpansionPanels/ExpansionPanel.svelte
@@ -1,75 +1,79 @@
-<script>
-  import { getContext } from 'svelte';
-  import { EXPANSION_PANELS } from './ExpansionPanels.svelte';
-  import { slide } from 'svelte/transition';
-  import Icon from '../Icon';
-  import down from '../../internal/Icons/down';
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { EXPANSION_PANELS } from './ExpansionPanels.svelte';
+	import { slide } from 'svelte/transition';
+	import Icon from '../Icon';
+	import down from '../../internal/Icons/down';
 
-  const { values, Disabled, selectPanel, index } = getContext(EXPANSION_PANELS);
+	const { values, Disabled, selectPanel, index } =
+		getContext(EXPANSION_PANELS);
 
-  // Classes to add to the panel.
-  let klass = '';
-  export { klass as class };
+	// Classes to add to the panel.
+	let klass = '';
+	export { klass as class };
 
-  // Options for the slide transition.
-  export let slideOpts = {};
+	// Options for the slide transition.
+	export let slideOpts = {};
 
-  // Make the panel readonly.
-  export let readonly = false;
+	// Make the panel readonly.
+	export let readonly = false;
 
-  // Disable the panel.
-  export let disabled = false;
+	// Disable the panel.
+	export let disabled: boolean | undefined = undefined;
 
-  // Styles to add to the panel.
-  export let style = null;
+	// Styles to add to the panel.
+	export let style: string | undefined = undefined;
 
-  const value = index();
-  let active = false;
+	const value = index();
+	let active = false;
 
-  function toggle() {
-    selectPanel(value);
-  }
+	function toggle() {
+		selectPanel(value);
+	}
 
-  // Inheriting the disabled value from parent.
-  $: disabled = $Disabled == null ? disabled : $Disabled;
+	// Inheriting the disabled value from parent.
+	$: disabled = $Disabled == null ? disabled : $Disabled;
 
-  // Checking if panel is active everytime the value has changed.
-  $: active = $values.includes(value);
+	// Checking if panel is active everytime the value has changed.
+	$: active = $values.includes(value);
 </script>
+
+<div
+	class="s-expansion-panel {klass}"
+	aria-expanded={active}
+	class:active
+	class:readonly
+	class:disabled
+	{style}
+>
+	<button
+		type="button"
+		class="s-expansion-panel__header"
+		tabindex={disabled ? -1 : undefined}
+		on:click={toggle}
+	>
+		<!-- Slot for the title of panel. -->
+		<slot name="header" />
+		<div class="s-expansion-panel__icon">
+			<!-- Slot for the icon at the right of the header. -->
+			<slot name="icon" {active}>
+				<Icon {disabled} path={down} rotate={active ? 180 : 0} />
+			</slot>
+		</div>
+	</button>
+	{#if active}
+		<div
+			class="s-expansion-panel__content"
+			transition:slide={slideOpts}
+			on:introstart
+			on:outrostart
+			on:introend
+			on:outroend
+		>
+			<slot />
+		</div>
+	{/if}
+</div>
 
 <style lang="scss" src="./ExpansionPanel.scss" global>
 </style>
-
-<div
-  class="s-expansion-panel {klass}"
-  aria-expanded={active}
-  class:active
-  class:readonly
-  class:disabled
-  {style}>
-  <button
-    type="button"
-    class="s-expansion-panel__header"
-    tabindex={disabled ? -1 : null}
-    on:click={toggle}>
-    <!-- Slot for the title of panel. -->
-    <slot name="header" />
-    <div class="s-expansion-panel__icon">
-      <!-- Slot for the icon at the right of the header. -->
-      <slot name="icon" {active}>
-        <Icon {disabled} path={down} rotate={active ? 180 : 0} />
-      </slot>
-    </div>
-  </button>
-  {#if active}
-    <div
-      class="s-expansion-panel__content"
-      transition:slide={slideOpts}
-      on:introstart
-      on:outrostart
-      on:introend
-      on:outroend>
-      <slot />
-    </div>
-  {/if}
-</div>
--- svelte-app/src/lib/Material/components/ExpansionPanels/ExpansionPanels.svelte
+++ svelte-app/src/lib/Material/components/ExpansionPanels/ExpansionPanels.svelte
@@ -1,91 +1,92 @@
-<script context="module">
-  export const EXPANSION_PANELS = {};
+<script context="module" lang="ts">
+	export const EXPANSION_PANELS = {};
 </script>
 
 <script>
-  import { createEventDispatcher, setContext } from 'svelte';
-  import { writable } from 'svelte/store';
+	import { createEventDispatcher, setContext } from 'svelte';
+	import { writable } from 'svelte/store';
 
-  // Classes to add to panel container.
-  let klass = '';
-  export { klass as class };
+	// Classes to add to panel container.
+	let klass = '';
+	export { klass as class };
 
-  // 0 based indices of the active panels.
-  export let value = [];
+	// 0 based indices of the active panels.
+	export let value: number[] = [];
 
-  // Make multiple panels active at the same time.
-  export let multiple = false;
+	// Make multiple panels active at the same time.
+	export let multiple = false;
 
-  // Make is necessary for at least one panel to be selected.
-  export let mandatory = false;
+	// Make is necessary for at least one panel to be selected.
+	export let mandatory = false;
 
-  // Accordion style panels.
-  export let accordion = false;
+	// Accordion style panels.
+	export let accordion = false;
 
-  // Make panels popout when active.
-  export let popout = false;
+	// Make panels popout when active.
+	export let popout = false;
 
-  // Make panels inset when active.
-  export let inset = false;
+	// Make panels inset when active.
+	export let inset = false;
 
-  // Remove shadow from panels.
-  export let flat = false;
+	// Remove shadow from panels.
+	export let flat = false;
 
-  // Remove border radius from panels.
-  export let tile = false;
+	// Remove border radius from panels.
+	export let tile = false;
 
-  // Disable all the panels.
-  export let disabled = null;
+	// Disable all the panels.
+	export let disabled: boolean | undefined = undefined;
 
-  // Styles to add to the panel container.
-  export let style = null;
+	// Styles to add to the panel container.
+	export let style: string | undefined = undefined;
 
-  const dispatch = createEventDispatcher();
-  const values = writable(value);
-  const Disabled = writable(disabled);
+	const dispatch = createEventDispatcher();
+	const values = writable(value);
+	const Disabled = writable(disabled);
 
-  $: values.set(value);
-  $: Disabled.set(disabled);
+	$: values.set(value);
+	$: Disabled.set(disabled as boolean);
 
-  let startIndex = -1;
+	let startIndex = -1;
 
-  setContext(EXPANSION_PANELS, {
-    values,
-    Disabled,
-    selectPanel: (index) => {
-      if (value.includes(index)) {
-        if (!(mandatory && value.length === 1)) {
-          value.splice(value.indexOf(index), 1);
-          value = value;
-          dispatch('change', { index, active: false });
-        }
-      } else {
-        if (multiple) {
-          value.push(index);
-          value = value;
-        } else {
-          value = [index];
-        }
-        dispatch('change', { index, active: true });
-      }
-    },
-    index: () => {
-      startIndex += 1;
-      return startIndex;
-    },
-  });
+	setContext(EXPANSION_PANELS, {
+		values,
+		Disabled,
+		selectPanel: (index: number) => {
+			if (value.includes(index)) {
+				if (!(mandatory && value.length === 1)) {
+					value.splice(value.indexOf(index), 1);
+					value = value;
+					dispatch('change', { index, active: false });
+				}
+			} else {
+				if (multiple) {
+					value.push(index);
+					value = value;
+				} else {
+					value = [index];
+				}
+				dispatch('change', { index, active: true });
+			}
+		},
+		index: () => {
+			startIndex += 1;
+			return startIndex;
+		},
+	});
 </script>
+
+<div
+	class="s-expansion-panels {klass}"
+	class:accordion
+	class:popout
+	class:inset
+	class:flat
+	class:tile
+	{style}
+>
+	<slot />
+</div>
 
 <style lang="scss" src="./ExpansionPanels.scss" global>
 </style>
-
-<div
-  class="s-expansion-panels {klass}"
-  class:accordion
-  class:popout
-  class:inset
-  class:flat
-  class:tile
-  {style}>
-  <slot />
-</div>

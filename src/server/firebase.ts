2e84926c4cc7ef0b7fa0870c022ca310956cab49
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-08-01 00:48:45 +0200

    removed admin

--- src/server/firebase.ts
+++ src/server/firebase.ts
@@ -1,11 +1,11 @@
 import admin from 'firebase-admin';
 
-export function getFirebaseApp() {
-	if (admin.apps?.length > 0) return admin.app();
-	return admin.initializeApp({
-		credential: admin.credential?.cert('./service-account.json'),
-	});
-}
+// export function getFirebaseApp() {
+// 	if (admin.apps?.length > 0) return admin.app();
+// 	return admin.initializeApp({
+// 		credential: admin.credential?.cert('./service-account.json'),
+// 	});
+// }
 
-export const firebase = getFirebaseApp();
-export const auth = firebase?.auth();
+// export const firebase = getFirebaseApp();
+// export const auth = firebase?.auth();

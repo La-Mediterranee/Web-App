21d087889f4afadb020458d37364b7cb47020361
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-09-27 12:49:03 +0200

    server disabled

--- src/lib/utils/firebase.ts
+++ src/lib/utils/firebase.ts
@@ -1,5 +1,5 @@
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { firebaseConfig } from './constants';
+// import { getApp, getApps, initializeApp } from 'firebase/app';
+// import { firebaseConfig } from './constants';
 // import { getAuth } from 'firebase/auth';
 // import { getFirestore } from 'firebase/firestore';
 

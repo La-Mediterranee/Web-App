db9b2e8be73b3447dafceaf3182a09f226cefe9a
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-09-27 12:56:10 +0200

    modal keys added

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -2,7 +2,7 @@ export const SHOP_OWNER = import.meta.env.VITE_SHOP_OWNER;
 export const SHOP_NAME = import.meta.env.VITE_SHOP_NAME;
 export const SHOP_LOGO = import.meta.env.VITE_SHOP_LOGO;
 export const SHOP_URL = import.meta.env.VITE_SHOP_URL;
-export const VITE_SERVER_URL = import.meta.VITE_SERVER_URL;
+export const VITE_SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 export const NODE_ENV = import.meta.env.NODE_ENV;
 
@@ -19,6 +19,9 @@ export const firebaseConfig = {
 	appId: import.meta.env.VITE_FIREBASE_APP_ID,
 	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+export const MODAL = 'MODAL';
+export const PRODUCT_MODAL = 'PRODUCT_MODAL';
 
 export const mediQueries = {
 	xs: '0px',

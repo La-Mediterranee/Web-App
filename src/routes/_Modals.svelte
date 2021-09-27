7a0a585bc5acdfee05c2633363ec958a7301eb32
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-09-27 12:49:43 +0200

    new modal context and removed unneseccary one

---
+++ src/routes/_Modals.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+	import { setContext } from 'svelte';
+
+	import { MODAL, PRODUCT_MODAL } from '$lib/utils/constants';
+
+	function open() {}
+	function close() {}
+</script>
+
+<script lang="ts">
+	import Modal from 'svelte-material-components/src/components/Dialog/Dialog.svelte';
+
+	let active = false;
+
+	setContext(MODAL, {
+		open,
+		close,
+	});
+
+	setContext(PRODUCT_MODAL, {
+		open,
+		close,
+	});
+</script>
+
+<slot />
+<Modal bind:active />

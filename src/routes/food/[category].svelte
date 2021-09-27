52402f9784847dbea5c9e9f348675ae416715189
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-09-27 12:50:22 +0200

    category for dynamic route

--- src/routes/food/[category].svelte
+++ src/routes/food/[category].svelte
@@ -1,10 +1,20 @@
 <script context="module" lang="ts">
-	export function load() {}
+	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+
+	export function load({ page }: LoadInput): LoadOutput {
+		return {
+			props: {
+				category: page.params.category,
+			},
+		};
+	}
 </script>
 
 <script lang="ts">
+	export let category: string;
+	console.log(category);
 </script>
 
-<style>
+<!-- <style>
 	/* your styles go here */
-</style>
+</style> -->

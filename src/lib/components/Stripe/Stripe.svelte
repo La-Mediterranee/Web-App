3f1e653b8219d94cd3d231a4f0f7ab6de6ec8c9b
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-09-27 18:54:08 +0200

    defered loading stripe

--- src/lib/components/Stripe/Stripe.svelte
+++ src/lib/components/Stripe/Stripe.svelte
@@ -1,50 +1,42 @@
 <script lang="ts">
-	import { onMount, setContext } from 'svelte';
 	import { writable } from 'svelte/store';
-	import { loadStripe } from '@stripe/stripe-js/pure.js';
+	import { onMount, setContext } from 'svelte';
+	import { loadStripe } from '@stripe/stripe-js';
 
 	import { STRIPE_PUBLIC_KEY } from '$lib/utils/constants';
 
-	import type { Stripe, StripeConstructor } from '@stripe/stripe-js';
+	import type { Stripe } from '@stripe/stripe-js';
 
 	const key = 'stripe';
 
-	if (typeof STRIPE_PUBLIC_KEY !== 'string') {
+	if (
+		typeof STRIPE_PUBLIC_KEY !== 'string' ||
+		typeof STRIPE_PUBLIC_KEY === 'undefined'
+	) {
 		throw new Error('VITE_STRIPE_PUBLIC_KEY must be added to .env');
 	}
 
 	const stripeStore = writable<Stripe | null>(null);
-	const { subscribe, set } = stripeStore;
 
-	setContext(key, {
-		subscribe,
-	});
+	try {
+		setContext(key, {
+			subscribe: stripeStore.subscribe,
+		});
 
-	function stripeLoaded(e: Event) {
-		const stripe: Stripe = (window.Stripe as StripeConstructor)(
-			STRIPE_PUBLIC_KEY
-		);
-		console.log('stripe loaded');
-		set(stripe);
-	}
-
-	onMount(async () => {
-		try {
-			const stripe = await loadStripe(STRIPE_PUBLIC_KEY);
-			console.log('stripe loaded');
-			set(stripe);
-		} catch (error) {
-			console.error(error);
-		}
-	});
+		onMount(async () => {
+			try {
+				const stripe = await loadStripe(STRIPE_PUBLIC_KEY);
+				console.info('stripe loaded', stripe);
+				stripeStore.set(stripe);
+			} catch (error) {
+				console.error(error);
+			}
+		});
+	} catch (error) {}
 </script>
 
-<!-- <svelte:head>
-	<script
-		defer
-		async
-		src="https://js.stripe.com/v3/"
-		on:load={stripeLoaded}></script>
-</svelte:head> -->
+<svelte:head>
+	<script defer async src="https://js.stripe.com/v3/"></script>
+</svelte:head>
 
 <slot stripe={stripeStore} />

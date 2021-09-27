20439c928a5f215d6b6c20dbf6056a28c84cc05f
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-09-27 12:47:51 +0200

    not needed stores

--- src/lib/firebase/index.ts
+++
@@ -1,7 +0,0 @@
-import Auth from './Auth.svelte';
-
-import FirebaseApp from './Firebase.svelte';
-
-// export const FirebaseApp = Firebase;
-export { FirebaseApp, Auth };
-// export default FirebaseApp;

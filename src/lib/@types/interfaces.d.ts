d3f4cd64d5af711775ae264083edce0a5e8af2f5
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-10-13 09:52:25 +0200

    id as property

--- src/lib/@types/interfaces.d.ts
+++ src/lib/@types/interfaces.d.ts
@@ -1,13 +1,21 @@
 import { Image } from './index';
 
 type SKU = string;
+type ID = string;
+
+type Variations = DeepReadonly<{
+	toppings?: string[];
+}>;
 
 interface Product {
+	readonly ID: ID;
 	readonly sku?: SKU;
 	readonly name: string;
+	readonly description: string;
 	readonly price: number;
-	readonly image: Image;
-	readonly categories: string[];
+	readonly image: readonly Image;
+	readonly categories: readonly string[];
+	readonly variations: readonly Variations;
 	readonly rating?: {
 		readonly value: number;
 		readonly count: number;

4663996832f0264f59dbd6aca07a1e1bf35ed655
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-10-14 19:03:01 +0200

    formatting and back function

--- src/lib/components/Checkout/checkout-sections/Payment.svelte
+++ src/lib/components/Checkout/checkout-sections/Payment.svelte
@@ -1,32 +1,28 @@
 <script context="module">
-	import type {
-		PaymentRequestShippingOption,
-		Stripe,
-		StripeCardElement,
-		StripeElements,
-	} from '@stripe/stripe-js';
+	import type { PaymentRequestShippingOption, Stripe, StripeCardElement, StripeElements } from '@stripe/stripe-js';
+
+	export type PaymentMethods = 'credit' | 'sofort' | 'bar';
 
 	const logos = {
 		amex: 'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/american-express.svg',
 		gPay: 'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/google-pay.svg',
 		eps: 'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/eps.svg',
-		applePay:
-			'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/apple-pay.svg',
+		applePay: 'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/apple-pay.svg',
 		klarna: 'https://x.klarnacdn.net/payment-method/assets/badges/generic/klarna.svg?locale=de_at',
 		maestro:
 			'https://raw.githubusercontent.com/aaronfagan/svg-credit-card-payment-icons/master/flat-rounded/maestro.svg',
-		maestroAlt:
-			'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/maestro-alt.svg',
+		maestroAlt: 'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/maestro-alt.svg',
 		visa: 'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/visa.svg',
 		visaFlatRounded:
 			'https://raw.githubusercontent.com/aaronfagan/svg-credit-card-payment-icons/master/flat-rounded/visa.svg',
-		mastercard:
-			'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/mastercard.svg',
+		mastercard: 'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/mastercard.svg',
 		mastercardAlt:
 			'https://raw.githubusercontent.com/datatrans/payment-logos/master/assets/logos/mastercard-alt.svg',
 		mastercardFlatRounded:
 			'https://raw.githubusercontent.com/aaronfagan/svg-credit-card-payment-icons/master/flat-rounded/mastercard.svg',
-	};
+	} as const;
+
+	const TAG = 'CHECKOUT';
 </script>
 
 <script lang="ts">
@@ -37,18 +33,17 @@
 	import Image from '$lib/components/Image/Image.svelte';
 	import CreditCard from '$lib/components/CreditCard/CreditCard.svelte';
 
-	type PaymentMethods = 'credit' | 'sofort' | 'bar';
-
 	export let elements: StripeElements;
 	export let value: number[] = [1];
 	export let currentValue = 1;
 
+	let form: HTMLFormElement;
 	let sum = 0;
-	let selected: HTMLFieldSetElement | null = null;
-	let paymentMethod: PaymentMethods = 'credit';
 	let tip = 'none';
 	let mounted = false;
-	let form: HTMLFormElement;
+	let paymentMethod: PaymentMethods = 'credit';
+	let selected: HTMLFieldSetElement | null = null;
+	let checked: 'summary' | 'details' | null = null;
 
 	const name = 'Zahlungsmethode';
 
@@ -56,58 +51,69 @@
 		mounted = true;
 	});
 
-	function checkOrderDeatils(e: Event) {
-		e.preventDefault();
-		const form = e.currentTarget as HTMLButtonElement;
-		console.log(form.formAction);
-		console.log(form);
+	function backToOrderDetails() {
+		console.debug(TAG, 'checkOrderDetails');
 
-		// value = [1];
-		// currentValue = 1;
+		value = [0];
+		currentValue = 0;
+	}
+
+	function goToSummary(e: SubmitEvent) {
+		console.debug(TAG, 'goToSummary');
+
+		const event = e as SubmitEvent;
+		const formData = new FormData(form);
+		for (const value of formData.values()) {
+			console.log(value);
+		}
+
+		value = [1];
+		currentValue = 1;
+	}
+
+	/**
+	 * Because only about 70% of Browser implement
+	 * the submitter property we have to add click handlers
+	 * to each button to distinguish between them
+	 *
+	 * @param e - the SubmitEvent (svelte tools has Event on `on:submit` instead of SubmitEvent)
+	 */
+
+	function checkNextStep(e: Event) {
+		console.debug(TAG, 'checkNextStep');
+
+		if (checked === 'summary') {
+			goToSummary(e as SubmitEvent);
+		} else {
+			backToOrderDetails();
+		}
+
+		// const submitter: HTMLButtonElement = (event.submitter as HTMLButtonElement)
+		// submitter?.value;
 	}
 </script>
 
-<form bind:this={form} action="/checkout?section=details" method="POST">
+<form bind:this={form} action="/checkout?section=details" method="POST" on:submit|preventDefault={checkNextStep}>
 	<section>
 		<h3>Trinkgeld hinzufügen</h3>
 		<fieldset id="tips">
-			<legend class="visually-hidden">
-				Trinkgeld Prozent Optionen
-			</legend>
+			<legend class="visually-hidden"> Trinkgeld Prozent Optionen </legend>
 			<div class="tip-container">
 				<div class="tip">
 					<label for="tip-5">5%</label>
-					<input
-						bind:group={tip}
-						id="tip-5"
-						type="radio"
-						class="visually-hidden-radio"
-						value="5"
-					/>
+					<input bind:group={tip} id="tip-5" type="radio" class="visually-hidden-radio" value="5" />
 					<div>{sum * 0.05}</div>
 					<div class="checked" />
 				</div>
 				<div class="tip">
 					<label for="tip-10">10%</label>
-					<input
-						bind:group={tip}
-						id="tip-10"
-						type="radio"
-						class="visually-hidden-radio"
-						value="10"
-					/>
+					<input bind:group={tip} id="tip-10" type="radio" class="visually-hidden-radio" value="10" />
 					<div>{sum * 0.1}</div>
 					<div class="checked" />
 				</div>
 				<div class="tip">
 					<label for="tip-15">15%</label>
-					<input
-						bind:group={tip}
-						id="tip-15"
-						type="radio"
-						class="visually-hidden-radio"
-						value="15"
-					/>
+					<input bind:group={tip} id="tip-15" type="radio" class="visually-hidden-radio" value="15" />
 					<div>{sum * 0.15}</div>
 					<div class="checked" />
 				</div>
@@ -125,9 +131,7 @@
 				</div>
 			</div>
 			<div class="custom-tip-container">
-				<TextField required outlined rounded>
-					Benutzerdefiniertes Trinkgeld
-				</TextField>
+				<TextField outlined rounded>Benutzerdefiniertes Trinkgeld</TextField>
 			</div>
 		</fieldset>
 	</section>
@@ -140,18 +144,12 @@
 
 				<div class="payment-processor">
 					<span>
-						<input
-							bind:group={paymentMethod}
-							type="radio"
-							id="credit"
-							value="credit"
-							{name}
-						/>
+						<input bind:group={paymentMethod} type="radio" id="credit" value="credit" {name} />
 					</span>
 					<div>
 						<label for="credit">Kreditkarte</label>
 						<ul>
-							{#each Array.from( [logos.visa, logos.mastercard, logos.maestroAlt, logos.amex] ) as logo}
+							{#each Array.from([logos.visa, logos.mastercard, logos.maestroAlt, logos.amex]) as logo}
 								<li>
 									<Image src={logo} width={38} height={24} />
 								</li>
@@ -170,13 +168,7 @@
 
 				<div class="payment-processor">
 					<span>
-						<input
-							bind:group={paymentMethod}
-							type="radio"
-							id="sofort"
-							value="sofort"
-							{name}
-						/>
+						<input bind:group={paymentMethod} type="radio" id="sofort" value="sofort" {name} />
 					</span>
 					<div>
 						<label for="sofort">Sofort Überweisung</label>
@@ -191,13 +183,7 @@
 
 				<div class="payment-processor">
 					<span>
-						<input
-							bind:group={paymentMethod}
-							type="radio"
-							id="bar"
-							value="bar"
-							{name}
-						/>
+						<input bind:group={paymentMethod} type="radio" id="bar" value="bar" {name} />
 					</span>
 					<div>
 						<label for="bar">Zahlung bei Abholung/Lieferung</label>
@@ -208,24 +194,21 @@
 				<Button
 					type="submit"
 					name="summary"
-					value="summary"
-					class="orange darken-1"
-					disabled
+					class="form-elements-color"
 					rounded
-					on:click={checkOrderDeatils}
+					on:click={() => {
+						checked = 'summary';
+					}}
 				>
 					Weiter zur Zusammenfassung
 				</Button>
 				<Button
 					type="submit"
 					name="details"
-					value="details"
 					formaction="/checkout?section=summary"
 					rounded
-					on:click={(e) => {
-						e.preventDefault();
-						value = [0];
-						currentValue = 0;
+					on:click={() => {
+						checked = 'details';
 					}}
 				>
 					Zurück zu den Lieferdetails
@@ -376,16 +359,16 @@
 		}
 
 		input {
-			color: gray;
-			border: 2px solid;
+			color: rgb(170, 155, 155);
+			// border: 2px solid;
+			// color: rgba(41, 34, 34, 0.4);
+			border: 3px solid;
 			width: 18px;
 			height: 18px;
 			transition: all 0.2s ease-in-out;
-			border-radius: 50%;
+			border-radius: 43%;
 
 			appearance: none;
-			-moz-appearance: none;
-			-webkit-appearance: none;
 			-webkit-font-smoothing: inherit;
 
 			&:checked {
@@ -429,6 +412,8 @@
 		width: 100%;
 		margin-top: 0.8em;
 		text-align: center;
+		display: flex;
+		flex-direction: row-reverse;
 
 		> :global(*) {
 			margin: 0.3em;

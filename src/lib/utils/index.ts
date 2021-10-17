f8ea6e5d8b05bffe050a0e74444cbeebf9d7c41d
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-10-17 21:25:40 +0200

    environment helper

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -4,3 +4,17 @@ export async function fetchPlus(path: RequestInfo, options = {}) {
 	alert(`This operation is not available while offline.`);
 	return { offline: true };
 }
+
+export const getGlobal = function () {
+	if (typeof self !== 'undefined') {
+		return self;
+	}
+
+	if (typeof window !== 'undefined') {
+		return window;
+	}
+
+	if (typeof global !== 'undefined') {
+		return global;
+	}
+};

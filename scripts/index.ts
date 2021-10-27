d2b2f89c454f952a977a29bb127ed8473fbfbb78
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-10-27 17:58:24 +0200

    added a lot and refactored

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,38 +1,208 @@
-import website from './config/website';
+import sharp from 'sharp';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { writeFileSync, readFileSync, existsSync, mkdirSync } from 'fs';
 
-import { writeFileSync, readFileSync } from 'fs';
-import { resolve } from 'path';
+import { getPath, getAppHtmlConfigFile, getSiteMapConfigFile } from './src/util/helper.js';
+import {
+	generateBrowserconfig,
+	generateManifest,
+	generateSitemap,
+	generateAppleImages,
+	generateFavicons,
+} from './src/generators/index.js';
+import {
+	DEFAULT_BROWSERCONFIG_PATH,
+	DEFAULT_MANIFEST_PATH,
+	DEFAULT_APPLE_SPLASH_PATH,
+	DEFAULT_FAVICONS_PATH,
+	DEFAULT_SITEMAP_PATH,
+	DEFAULT_APPLE_ICON_PATH,
+	DEFAULTS,
+} from './src/util/consts.js';
+
+// import os from 'os';
+// import WorkerPool from './src/util/worker-pool.js';
+//const pool = new WorkerPool(os.cpus().length, '');
+
+// const __filename: string = fileURLToPath(import.meta.url);
+// const __dirname: string = dirname(__filename!);
 
 main();
 
-async function main(lang = 'de') {
-	const pollyfills = `<script>
-			${readFileSync(resolve(__dirname, './pollyfills.js'))}
-	</script>`;
+async function main() {
+	const appHtmlConfig = await getAppHtmlConfigFile('src/config');
+	// console.log('PWAConfig:', appHtmlConfig);
 
-	const head = `${pollyfills}`;
+	if (!appHtmlConfig.appName) {
+		throw new Error('missing appName in PWA Config');
+	}
 
-	const html = `
-	<!DOCTYPE html>
-	<html lang="${lang}">
-		${writeHead(head)}
+	if (!appHtmlConfig.iconPath) {
+		throw new Error('missing iconPath in PWA Config');
+	}
 
-		<body>
-			<div id="svelte">%svelte.body%</div>
-			${firebaseA11y()}	
-		</body>
-	</html>`;
+	const padding = appHtmlConfig.padding || DEFAULTS.padding;
+	const iconBuffer = readFileSync(appHtmlConfig.iconPath);
 
-	writeFileSync(resolve(__dirname, './index.html'), html);
-	return 0;
+	// PWA Manifest
+	const pwaManifestConfig = appHtmlConfig.pwaManifest;
+	const pwaManifestIconBuffer = pwaManifestConfig?.iconPath
+		? readFileSync(getPath(pwaManifestConfig?.iconPath))
+		: iconBuffer;
+	const manifestPath = pwaManifestConfig?.manifestOutPath || DEFAULT_MANIFEST_PATH;
+
+	await generateManifest({
+		icon: sharp(pwaManifestIconBuffer),
+		iconsOutDir: pwaManifestConfig?.iconsOutDir,
+		padding: padding,
+		manifestOutPath: manifestPath,
+		manifest: {
+			name: appHtmlConfig.appName,
+			themeColor: appHtmlConfig.themeColor,
+			...(pwaManifestConfig?.manifest || {}),
+		},
+	});
+
+	// Apple Images
+	const appleConfig = appHtmlConfig.apple;
+	const appleIconBuffer = appleConfig?.iconPath ? readFileSync(getPath(appleConfig?.iconPath)) : iconBuffer;
+	const appleTags = await generateAppleImages(sharp(appleIconBuffer), padding, {
+		themeColor: appHtmlConfig.themeColor,
+		themeColorDark: appHtmlConfig.themeColorDark,
+		iconsOutPath: appHtmlConfig.apple?.iconsOutPath,
+		splashScreensOutPath: appHtmlConfig.apple?.splashScreensOutPath,
+	});
+
+	// Favicons
+	const faviconConfig = appHtmlConfig.favicon;
+	if (!Array.isArray(faviconConfig)) {
+		const faviconImageBuffer = faviconConfig?.iconPath
+			? readFileSync(getPath(faviconConfig?.iconPath))
+			: iconBuffer;
+		generateFavicons(sharp(faviconImageBuffer));
+	}
+
+	// Microsoft Browserconfig
+	const browserconfig = appHtmlConfig.browserconfig;
+	if (browserconfig) {
+		console.log('Writing to filesystem path:', getPath(browserconfig.outPath || DEFAULT_BROWSERCONFIG_PATH));
+		generateBrowserconfig(browserconfig);
+		console.log('Finished Browserconfig');
+	}
+
+	console.log('Writing app.html');
+
+	writeAppHTML({
+		headConfig: {
+			pwaName: appHtmlConfig.appName,
+			startUrl: appHtmlConfig.startUrl,
+			...appHtmlConfig.headConfig,
+		},
+	});
+
+	console.log('Finished Writing');
+
+	const sitemapConfig = await getSiteMapConfigFile('src/config');
+	// console.log('Sitemap:', sitemapConfig);
+
+	// // Sitemap
+	// console.log('Writing to filesystem path:', DEFAULT_SITEMAP_PATH);
+	// generateSitemap(sitemapConfig);
 }
 
-function writeHead(head?: string) {
-	return `
+interface HeadOptions {
+	pwaName: string;
+	startUrl?: string;
+	pwa?: boolean;
+	shrinkToFit?: boolean;
+	apple?: ApplePWA;
+	externalStyleSheets?: readonly string[];
+	preConnections?: readonly string[];
+	dnsPrefetches?: readonly string[];
+	transform?: ((head: string) => string) | undefined;
+}
+
+function writeAppHTML(config: { lang?: string; body?: string; headConfig: HeadOptions }) {
+	const { lang = 'de', body = '', headConfig } = config;
+
+	const html = `
+<!DOCTYPE html>
+<html lang="${lang}">
+${writeHead(headConfig)}
+${writeBody(body)}
+</html>`;
+
+	if (!existsSync(getPath('src'))) {
+		mkdirSync(getPath('src'));
+	}
+
+	writeFileSync(getPath('src/app.html'), html);
+}
+
+function writeHead(options: HeadOptions) {
+	const {
+		pwaName,
+		shrinkToFit = true,
+		pwa = true,
+		startUrl = DEFAULTS.startUrl,
+		apple,
+		dnsPrefetches = [],
+		externalStyleSheets = [],
+		preConnections = [],
+		transform = (head) => head,
+	} = options;
+
+	const isPWA = pwa ? 'yes' : 'no';
+	const fullscreen = apple?.fullscreen || true ? 'yes' : 'no';
+	const statusBarStyle = apple?.statusBarStyle || 'default';
+
+	const polyfillsFile = readFileSync(getPath('./polyfills.js'));
+	const polyfills =
+		polyfillsFile.length !== 0
+			? `
+	<script>
+	${polyfillsFile}
+	</script>`
+			: '';
+
+	const head = `
 	<head>
-		${head}
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" ${!shrinkToFit ? 'shrink-to-fit=no ' : ''}/>
+		
+		<meta name="application-name" content="${pwaName}" />
+		<meta name="apple-mobile-web-app-title" content="${pwaName}" />
+		<meta name="mobile-web-app-capable" content=${isPWA} />
+		<meta name="apple-mobile-web-app-capable" content=${isPWA} />
+		<meta name="apple-touch-fullscreen" content="${fullscreen}" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="${statusBarStyle}" />
+		${startUrl && `<meta name="msapplication-starturl" content="${startUrl}" />`}
+
+		${preConnections
+			.map((con) => {
+				return `
+		<link rel="preconnect" href="${con}" />
+		<link rel="dns-prefetch" href="${con}" />`;
+			})
+			.join('\n')}
+		${dnsPrefetches.map((prefetch) => `<link rel="dns-prefetch" href="${prefetch}" />`)}
+		${externalStyleSheets.map((href) => `<link rel="stylesheet" href="${href}" />`)}
+		${polyfills}
 		%svelte.head%
 	</head>`;
+
+	return transform(head);
+}
+
+function writeBody(body: string = '') {
+	return `
+	<body>
+		<div id="svelte">%svelte.body%</div>
+		${body}
+		${firebaseA11y()}	
+	</body>`;
 }
 
 function firebaseA11y() {
@@ -51,16 +221,4 @@ function firebaseA11y() {
 			});
 			observer.observe(document.body, { childList: true });
 		</script>`;
-}
-
-function preConnections() {
-	return `
-		<link rel="preconnect" href="https://fonts.gstatic.com" />
-		<link rel="dns-prefetch" href="https://fonts.gstatic.com" />
-
-		<link rel="preconnect" href="https://fonts.googleapis.com" />
-		<link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-
-		<link rel="preconnect" href="https://www.googletagmanager.com" />
-		<link rel="dns-prefetch" href="https://www.googletagmanager.com" />`;
 }

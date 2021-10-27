05baab55775138b5567001e640bf221902eebb90
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-10-27 18:03:00 +0200

    reexport

---
+++ scripts/src/generators/index.ts
@@ -0,0 +1,5 @@
+export { generateSitemap } from './sitemap.js';
+export { generateManifest } from './manifest.js';
+export { generateFavicons } from './favicons.js';
+export { generateAppleImages } from './apple-images.js';
+export { generateBrowserconfig } from './browserconfig.js';

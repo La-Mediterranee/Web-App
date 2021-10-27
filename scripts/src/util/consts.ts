200d82c97d77bdbf7ce83e3820b5ad892c454fa4
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-10-27 18:06:59 +0200

    some defaults

---
+++ scripts/src/util/consts.ts
@@ -0,0 +1,16 @@
+export const DEFAULT_ICON_PATH = 'static/icons';
+export const DEFAULT_APPLE_SPLASH_PATH = `${DEFAULT_ICON_PATH}/splash`;
+export const DEFAULT_PWA_ICONS_PATH = `${DEFAULT_ICON_PATH}/manifest`;
+export const DEFAULT_FAVICONS_PATH = `${DEFAULT_ICON_PATH}/favicons`;
+export const DEFAULT_APPLE_ICON_PATH = `${DEFAULT_ICON_PATH}/apple-touch`;
+
+export const DEFAULT_SITEMAP_PATH = 'static/sitemap.xml';
+export const DEFAULT_MANIFEST_PATH = 'static/manifest.webmanifest';
+export const DEFAULT_BROWSERCONFIG_PATH = 'static/browserconfig.xml';
+
+export const DEFAULTS = {
+	themeColor: '#fff',
+	backgroundColor: '#fff',
+	padding: 0.2,
+	startUrl: '/',
+};

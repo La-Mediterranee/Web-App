ebf15234192a1d6a9594cff92df1bc3761f70592
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-11-10 20:48:23 +0100

    i18n paths

---
+++ src/routes/[lang]/index.svelte
@@ -0,0 +1,13 @@
+<script context="module" lang="ts">
+	import LL from '$i18n/i18n-svelte';
+	export function load() {}
+</script>
+
+<script>
+    
+</script>
+
+<!-- markup (zero or more items) goes here -->
+<style>
+
+</style>

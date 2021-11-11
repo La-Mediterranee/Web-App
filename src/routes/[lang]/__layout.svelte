a31850fe1697327b356134e2673f59f4287453c2
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-11-11 22:08:06 +0100

    moved ga

--- src/routes/[lang]/__layout.svelte
+++ src/routes/[lang]/__layout.svelte
@@ -3,10 +3,8 @@
 	import { onMount } from 'svelte';
 
 	import { RTL_LANGS } from '$i18n/utils';
-	import { baseLocale, locales } from '$i18n/i18n-util';
 	import { mobileNavItems, desktopNavItems } from '$utils/navItems';
 	import { registerServiceWorker } from '$lib/pwa/register-sw';
-	import { getGlobal, replaceLocaleInUrl } from '$lib/utils';
 
 	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
 	import type { Locales } from '$i18n/i18n-types';
@@ -23,18 +21,18 @@
 </script>
 
 <script lang="ts">
+	import { metatags } from '$lib/stores/metatags';
 	import Modals from './_Dialogs.svelte';
+
+	import t from '$i18n/i18n-svelte';
 
 	import LDTag from '$lib/components/LDTag';
 	import Navbar from '$lib/components/Navbar';
 	import Footer from '$lib/components/Footer';
 	import Statusbar from '$lib/components/Statusbar';
 	import Tabbar from '$lib/components/Tabbar';
-	import Installprompt from '$lib/components/Prompts/Installprompt';
-
-	import { metatags } from '$lib/stores/metatags';
-	import UpdatePrompt from '$lib/components/Prompts/SericeWorker/UpdatePrompt.svelte';
-	import LL from '$i18n/i18n-svelte';
+	import Installprompt from '$lib/pwa/Prompts/Installprompt';
+	import UpdatePrompt from '$lib/pwa/Prompts/SericeWorker/UpdatePrompt.svelte';
 	import LocaleSwitcher from '$lib/components/LocaleSwitcher/LocaleSwitcher.svelte';
 
 	export let rtl: boolean;
@@ -42,15 +40,6 @@
 	let online: boolean = true;
 
 	onMount(async () => {
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag(...args: unknown[]) {
-			window.dataLayer.push(args as never);
-		}
-
-		gtag('js', new Date());
-		gtag('config', 'GA_MEASUREMENT_ID');
-
 		if (!dev) {
 			registerServiceWorker();
 		}
@@ -78,10 +67,9 @@
 	{/each}
 </svelte:head> -->
 
-<!-- <Providers> -->
 <Modals>
 	<div id="mainContent">
-		<Statusbar message={$LL.connectionStatus()} {online} />
+		<Statusbar message={$t.connectionStatus()} {online} />
 		<Navbar routes={desktopNavItems} />
 		<main>
 			<Installprompt installSource={'LayoutInstallButton'} />
@@ -93,4 +81,3 @@
 		<Footer />
 	</div>
 </Modals>
-<!-- </Providers> -->

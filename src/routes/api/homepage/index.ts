1584b66086526dcdda187caeef3078a36dfbc2eb
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-11-12 21:05:14 +0100

    endpoint

---
+++ src/routes/api/homepage/index.ts
@@ -0,0 +1,63 @@
+import type { EndpointOutput, Request } from '@sveltejs/kit';
+
+type GetRequest<Locals = Record<string, any>, Input = unknown> = Request<Locals, Input>;
+
+import { promisify } from 'util';
+import { randomUUID } from 'crypto';
+
+import type { Product } from 'types/product';
+import type { ServerRequest } from '@sveltejs/kit/types/hooks';
+
+interface GetBody {
+	product?: Product;
+}
+
+export async function get({ locals }: ServerRequest<Record<string, any>, unknown>): Promise<EndpointOutput> {
+	return {
+		body: JSON.stringify(homepage()),
+	};
+}
+
+interface HomepageSection<T> {
+	title: string;
+	body: T;
+}
+
+export interface HomepageProps {
+	sections: HomepageSection<unknown>[];
+	bestseller: Product[];
+}
+
+function homepage(): HomepageProps {
+	const p: Product = {
+		ID: randomUUID(),
+		name: 'Hamburger',
+		description: '',
+		price: 4.5,
+		categories: ['burger'],
+		image: { src: '/burger.webp', alt: 'Bild von einem Burger' },
+		variations: {
+			toppings: ['Beilagen', 'Saucen'],
+		},
+	};
+
+	const bestsellerSection: HomepageSection<Product[]> = {
+		title: 'Bestseller',
+		body: Array(10).fill(p),
+	};
+
+	const foodSection: HomepageSection<Product[]> = {
+		title: 'Essen',
+		body: Array(12).fill(p),
+	};
+
+	const drinksSection: HomepageSection<Product[]> = {
+		title: 'Getränke',
+		body: Array(7).fill(p),
+	};
+
+	return {
+		sections: [bestsellerSection, foodSection, drinksSection],
+		bestseller: Array(10).fill(p),
+	};
+}

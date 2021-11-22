52bf2d34de6d52e5c30c6ca4dd456cfddda6e0a6
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-11-22 08:03:24 +0100

    firebase messaging

--- src/env.d.ts
+++ src/env.d.ts
@@ -18,4 +18,5 @@ interface ImportMetaEnv {
 	readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
 	readonly VITE_FIREBASE_APP_ID: string;
 	readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+	readonly VITE_FIREBASE_VAPID_KEY: string;
 }

227ba0a41e6219214c1a3c252ffcd918ba39a2c2
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-11-22 08:20:58 +0100

    throttle function

--- src/lib/utils/throttle.ts
+++ src/lib/utils/throttle.ts
@@ -1,26 +1,37 @@
-const throttle = (
-	func: () => void,
-	limit: number,
-	setIsInThrottle?: (value?: boolean) => void
-): (() => void) => {
+const throttleWithContext = <T>(fn: Function, wait: number = 300) => {
 	let inThrottle: boolean;
-	return function () {
-		const args = arguments;
+	let lastFn: ReturnType<typeof setTimeout>;
+	let lastTime: number;
+
+	return function (this: T) {
 		const context = this;
+		const args = arguments;
+
 		if (!inThrottle) {
-			func.apply(context, args);
+			fn.apply(context, args);
+			lastTime = Date.now();
 			inThrottle = true;
-			if (typeof setIsInThrottle === 'function') {
-				setIsInThrottle(true);
-			}
-			setTimeout(() => {
-				inThrottle = false;
-				if (typeof setIsInThrottle === 'function') {
-					setIsInThrottle(false);
+		} else {
+			clearTimeout(lastFn);
+			lastFn = setTimeout(() => {
+				if (Date.now() - lastTime >= wait) {
+					fn.apply(context, args);
+					lastTime = Date.now();
 				}
-			}, limit);
+			}, Math.max(wait - (Date.now() - lastTime), 0));
 		}
 	};
 };
+
+function throttle(fn: Function, wait: number = 300) {
+	let inThrottle: boolean;
+
+	return (...args: unknown[]) => {
+		if (!inThrottle) {
+			fn(args);
+			inThrottle = true;
+		}
+	};
+}
 
 export default throttle;

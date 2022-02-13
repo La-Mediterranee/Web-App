5523fb32e9c8c8538275e1c61e7906dcfc1896ea
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2022-02-13 19:20:35 +0100

    added: cookierefresh, more locale parser

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,19 +1,54 @@
 import jwt_decode from 'jwt-decode';
 
 import { sequence } from '@sveltejs/kit/hooks';
-// import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors';
 
 import { RTL_LANGS } from '$i18n/utils';
 import { detectLocale } from '$i18n/i18n-util';
 
 import { parse } from '$lib/server/cookie';
 import { auth } from '$lib/server/firebase';
+import { refreshSessionCookie, setCookie } from '$lib/server/helper';
 import { attachCsrfToken } from '$lib/server/csrf';
-import type { LocaleDetector } from 'typesafe-i18n/detectors';
 
+import type { AuthError } from 'firebase/auth';
 import type { Handle, RequestEvent } from '@sveltejs/kit';
 import type { Locale } from 'typesafe-i18n/types/core';
-import type { AuthError } from 'firebase/auth';
+import type { LocaleDetector } from 'typesafe-i18n/detectors';
+
+const REGEX_ACCEPT_LANGUAGE_SPLIT = /;|,/;
+
+export const initAcceptLanguageHeaderDetector =
+	(request: Request, headerKey = 'accept-language'): LocaleDetector =>
+	(): Locale[] =>
+		(request.headers.get(headerKey) as string)
+			?.split(REGEX_ACCEPT_LANGUAGE_SPLIT)
+			.filter(part => !part.startsWith('q'))
+			.map(part => part.trim())
+			.filter(part => part !== '*')
+			.filter(value => value !== '') || [];
+
+function initRequestCookiesDetector(request: Request, headerKey = 'pref-locale'): LocaleDetector {
+	const prefLang = request.headers.get(headerKey);
+	return () => (prefLang ? [prefLang] : []);
+}
+
+function initCustomClaims(event: RequestEvent): LocaleDetector {
+	const locale = event.locals.user?.locale;
+	return () => (locale ? [locale] : []);
+}
+
+const parseLocale: Handle = async ({ event, resolve }) => {
+	if (!event.locals.locale) {
+		// set the preffered language
+		const customClaims = initCustomClaims(event);
+		const requestCookies = initRequestCookiesDetector(event.request);
+		const acceptLanguageDetector = initAcceptLanguageHeaderDetector(event.request);
+
+		event.locals.locale = detectLocale(customClaims, requestCookies, acceptLanguageDetector);
+	}
+
+	return resolve(event);
+};
 
 const cookieParser: Handle = async ({ event, resolve }) => {
 	const cookies = parse<Cookies>(event.request.headers.get('cookie') || '');
@@ -50,28 +85,37 @@ const parseUser: Handle = async ({ event, resolve }) => {
 		event.locals.user = null;
 	}
 
-	return resolve(event);
-};
-
-const last: Handle = async ({ event, resolve }) => {
-	try {
-		const cookies = event.locals.cookies;
-		event.locals.locale = cookies['pref-locale'];
-	} catch (error) {
-		console.error(error);
-	}
-
 	const response = await resolve(event);
+
+	if (event.locals.user) {
+		const expiration = new Date(1970, 0, 1).setSeconds(event.locals.user.exp);
+		const current = Date.now();
+
+		if (Math.abs(expiration - current) / 36e5 <= 24) {
+			const days = 14;
+			const expiresIn = days * 60 * 60 * 24 * 1000;
+
+			const newCookie = await refreshSessionCookie(event.locals.user.uid, expiresIn);
+
+			setCookie(response, 'sessionId', newCookie, {
+				maxAge: expiresIn / 1000,
+				httpOnly: true,
+				secure: true,
+				path: '/',
+				// domain: ".",
+			});
+		}
+	}
 
 	return response;
 };
 
 export const handle = sequence(
 	cookieParser,
-	attachCsrfToken('/', 'csrfToken'),
-	browserChecker,
 	parseUser,
-	last,
+	parseLocale,
+	browserChecker,
+	attachCsrfToken('/', 'csrfToken'),
 );
 
 // export interface User {
@@ -81,32 +125,7 @@ export const handle = sequence(
 // 	displayName: string;
 // }
 
-const REGEX_ACCEPT_LANGUAGE_SPLIT = /;|,/;
-
-export const initAcceptLanguageHeaderDetector =
-	(request: Request, headerKey = 'accept-language'): LocaleDetector =>
-	(): Locale[] =>
-		(request.headers.get(headerKey) as string)
-			?.split(REGEX_ACCEPT_LANGUAGE_SPLIT)
-			.filter(part => !part.startsWith('q'))
-			.map(part => part.trim())
-			.filter(part => part !== '*')
-			.filter(value => value !== '') || [];
-
-function initRequestCookiesDetector(request: Request, headerKey = 'pref-locale'): LocaleDetector {
-	const prefLang = request.headers.get(headerKey);
-	return () => (prefLang ? [prefLang] : []);
-}
-
-export function getSession(event: RequestEvent): App.Session {
-	if (!event.locals.locale) {
-		// set the preffered language
-		const requestCookies = initRequestCookiesDetector(event.request);
-		const acceptLanguageDetector = initAcceptLanguageHeaderDetector(event.request);
-
-		event.locals.locale = detectLocale(requestCookies, acceptLanguageDetector);
-	}
-
+export async function getSession(event: RequestEvent): Promise<App.Session> {
 	return {
 		user: getUser(event.locals),
 		locale: event.locals.locale,

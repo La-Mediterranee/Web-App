7de3becfbeed50a89df1c033bb2bab6113c97c49
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2022-02-13 19:22:56 +0100

    using head instead of post, more appropriate

--- src/routes/api/session/logout.ts
+++ src/routes/api/session/logout.ts
@@ -1,19 +1,28 @@
 import { clearCookie } from '$lib/server/helper';
 
 import type { EndpointOutput, RequestEvent } from '@sveltejs/kit';
-import type { SessionLogin } from '../session/login';
 
-export async function post(req: RequestEvent): Promise<EndpointOutput> {
+// export async function post(req: RequestEvent): Promise<EndpointOutput> {
+// 	req.locals.user = null;
+
+// 	const response = {
+// 		// status: 302,
+// 		// headers: {
+// 		// 	location: '/',
+// 		// },
+// 	};
+
+// 	clearCookie(response, 'sessionId');
+
+// 	return response;
+// }
+
+export function head(req: RequestEvent): EndpointOutput {
 	req.locals.user = null;
 
-	const response = {
-		status: 302,
-		headers: {
-			location: '/',
-		},
-	};
+	const response = {};
 
-	clearCookie(response, 'session_id');
+	clearCookie(response, 'sessionId');
 
 	return response;
 }

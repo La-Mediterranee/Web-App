51c317e28664325ee4fbeda41a0c52815f3c86fa
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2022-02-15 17:34:05 +0100

    copied from outer layout

--- src/routes/[...locale]/__layout.svelte
+++ src/routes/[...locale]/__layout.svelte
@@ -1,7 +1,53 @@
 <script context="module" lang="ts">
+	import { initI18n } from '$i18n/i18n-svelte';
+	import { baseLocale } from '$i18n/i18n-util';
+	import { RTL_LANGS, locales } from '$i18n/utils';
+
+	// import { registerServiceWorker } from '$lib/pwa/register-sw';
+	import { replaceLocaleInUrl } from '$lib/utils';
+
 	import type { Locales } from '$i18n/i18n-types';
 	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
 
+	// export async function load({ params, session, url }: LoadInput): Promise<LoadOutput> {
+	// 	const locale = params.locale as Locales;
+
+	// 	console.log(`\nlocale: ${JSON.stringify(params)}`);
+	// 	// console.log(params.locale?.split('/')?.length > 1);
+
+	// 	if (params.locale?.split('/')?.length > 1) {
+	// 		return {
+	// 			status: 400,
+	// 		};
+	// 	}
+
+	// 	// redirect to preferred language if user comes from page root
+	// 	if (locale !== session.locale && session.locale !== baseLocale) {
+	// 		return {
+	// 			status: 302,
+	// 			redirect: `/${session.locale}`,
+	// 		};
+	// 	}
+
+	// 	// redirect to base locale if language is not present
+	// 	if ((locale as string) !== '/' && (locale as string) !== '' && !locales.has(locale)) {
+	// 		return {
+	// 			status: 302,
+	// 			redirect: replaceLocaleInUrl(url.pathname, ''),
+	// 			// redirect: '/',
+	// 		};
+	// 	}
+
+	// 	// load dictionary data
+	// 	await initI18n(session.locale);
+
+	// 	return {
+	// 		props: {
+	// 			lang: session.locale,
+	// 			dir: RTL_LANGS.has(locale) ? 'rtl' : 'ltr',
+	// 		},
+	// 	};
+	// }
 	export async function load({ session }: LoadInput): Promise<LoadOutput> {
 		const output: LoadOutput = {
 			props: {},

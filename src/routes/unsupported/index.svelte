471d4a7f2667fab6e4222dd0aa3f168cfbb46d53
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2022-02-15 17:29:59 +0100

    unsupported route

---
+++ src/routes/unsupported/index.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import LL, { initI18n } from '$i18n/i18n-svelte';
+	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+
+	export async function load({ session }: LoadInput): Promise<LoadOutput> {
+		await initI18n(session.locale);
+
+		return {};
+	}
+</script>
+
+<div>
+	{$LL.footer.about({ shopname: 'La' })}
+</div>
+
+<style>
+	/* your styles go here */
+</style>

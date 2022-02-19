ca05a990d2b0156bba25e377bd2a5b3a398ef67a
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2022-02-20 00:51:09 +0100

    new form component

---
+++ src/lib/components/Forms/Checkbox.svelte
@@ -0,0 +1,31 @@
+<svelte:options immutable />
+
+<script lang="ts">
+	export let id: string;
+	export let name: string;
+	export let value: string;
+	export let checked = false;
+	export let required = false;
+</script>
+
+<input type="checkbox" class="theme-radio-btn" {id} {name} {value} {checked} {required} />
+
+<style lang="scss" global>
+	.theme-radio-btn {
+		color: rgba(41, 34, 34, 0.4);
+		border: 3px solid;
+		width: 20px;
+		height: 20px;
+		transition: all 0.2s ease-in-out;
+		border-radius: 41%;
+
+		appearance: none;
+		-webkit-font-smoothing: inherit;
+		-moz-osx-font-smoothing: inherit;
+
+		&:checked {
+			border-color: var(--accent-color, orange);
+			border-width: 9px;
+		}
+	}
+</style>

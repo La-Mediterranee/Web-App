4db5981a9c204a7d7c82f83a96a482b7e3648b8d
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2022-02-20 00:49:29 +0100

    fixed footer

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -1,25 +1,6 @@
 <script></script>
 
 <footer>
-	<!-- <svg
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 1440 500"
-		fill="url(#MyGradient)"
-		preserveAspectRatio="none"
-	>
-		<defs>
-			<linearGradient id="MyGradient">
-				<stop offset="0" stop-color="var(--top1)" />
-				<stop offset="100%" stop-color="var(--top2)" />
-			</linearGradient>
-		</defs>
-		<path
-			height="180"
-			fill-opacity="1"
-			d="M0,128L48,144C96,160,192,192,288,197.3C384,203,480,181,576,149.3C672,117,768,75,864,85.3C960,96,1056,160,1152,176C1248,192,1344,160,1392,144L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-		/>
-		<rect y="319" width="1440" height="100%" />
-	</svg> -->
 	<svg
 		width="1440"
 		height="237"
@@ -70,63 +51,62 @@
 	</a>
 </footer>
 
-<style lang="scss">
+<style lang="scss" global>
 	@use 'variables' as *;
 
 	footer {
 		display: flex;
-		width: 100%;
-
 		position: relative;
 		text-align: center;
-		// padding-top: clamp(10px, 6%, 40px);
+
+		width: 100%;
 		height: 100%;
-		margin-top: 5.5em;
-	}
+		margin-top: 5em;
 
-	svg {
-		display: block;
-		position: absolute;
-		top: 0;
-		height: 130px;
-		width: 100%;
-		transform: translateY(-5em);
+		svg {
+			display: block;
+			position: absolute;
+			top: 0;
+			height: 130px;
+			width: 100%;
+			transform: translateY(-5em);
 
-		// transform: translateY(-4em);
+			// transform: translateY(-4em);
 
-		path {
-			stroke: none;
+			path {
+				stroke: none;
+			}
+
+			rect {
+				stroke: none;
+			}
 		}
 
-		rect {
-			stroke: none;
+		ul {
+			margin: 0.3em;
 		}
-	}
 
-	ul {
-		margin: 0.3em;
-	}
+		li {
+			list-style: none;
+		}
 
-	li {
-		list-style: none;
-	}
+		.content {
+			position: relative;
+			z-index: 1;
+			display: block;
+			background: var(--theme-app-bar);
+			padding-bottom: var(--tab-bar-active-height);
 
-	.content {
-		position: relative;
-		z-index: 1;
-		display: block;
-		background: var(--theme-app-bar);
-		padding-bottom: var(--tab-bar-active-height);
+			width: 100%;
 
-		width: 100%;
+			// @media (min-width: 325px) {
+			// 	margin-bottom: clamp(0px, 1%, 20px);
+			// }
 
-		// @media (min-width: 325px) {
-		// 	margin-bottom: clamp(0px, 1%, 20px);
-		// }
-
-		@media screen and (min-width: map-get($map: $breakpoints, $key: sm)) {
-			display: flex;
-			justify-content: space-around;
+			@media screen and (min-width: map-get($map: $breakpoints, $key: sm)) {
+				display: flex;
+				justify-content: space-around;
+			}
 		}
 	}
 </style>

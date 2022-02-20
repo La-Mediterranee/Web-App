e00123989e23acd5da76ddee530c7bfb35b7a6cb
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2022-02-20 19:36:39 +0100

    removed: unused import

--- src/lib/components/Customer/Profile/Profile.svelte
+++ src/lib/components/Customer/Profile/Profile.svelte
@@ -4,7 +4,6 @@
 	import Button from 'svelty-material/components/Button/Button.svelte';
 	import Select from 'svelty-material/components/Select/Select.svelte';
 
-	import { onMount } from 'svelte';
 	import { session } from '$app/stores';
 	import { goto } from '$app/navigation';
 
